d9374782ae630836bb0ecd71c6afc3c9e42c48d7
zjc-baotou <3020688632@qq.com>
2025-05-03 10:14:45 +0800

    fact: 增加流式输出

--- harryPorter/src/main/java/zjc/ai/OllamaConfig.java
+++ harryPorter/src/main/java/zjc/ai/OllamaConfig.java
@@ -2,6 +2,7 @@ package zjc.ai;
 
 import dev.langchain4j.http.client.spring.restclient.SpringRestClientBuilder;
 import dev.langchain4j.model.ollama.OllamaChatModel;
+import dev.langchain4j.model.ollama.OllamaStreamingChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,6 +18,16 @@ public class OllamaConfig {
                 .httpClientBuilder(new SpringRestClientBuilder())
                 .timeout(Duration.ofSeconds(30)) // 超时时间
                 .maxRetries(3) // 超时最大重试次数
+                .build();
+    }
+
+    @Bean
+    public OllamaStreamingChatModel ollamaStreamingChatModel(){
+        return OllamaStreamingChatModel.builder()
+                .baseUrl("http://localhost:11434")
+                .modelName("deepseek-r1:7b")
+                .httpClientBuilder(new SpringRestClientBuilder())
+                .timeout(Duration.ofSeconds(30)) // 超时时间
                 .build();
     }
 }
--- harryPorter/src/main/java/zjc/ai/OllamaController.java
+++ harryPorter/src/main/java/zjc/ai/OllamaController.java
@@ -4,10 +4,12 @@ import dev.langchain4j.data.message.TextContent;
 import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.chat.request.ChatRequest;
 import dev.langchain4j.model.chat.response.ChatResponse;
+import dev.langchain4j.model.chat.response.StreamingChatResponseHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 import javax.annotation.Resource;
 
@@ -24,5 +26,35 @@ public class OllamaController {
                 .build();
         ChatResponse chat = ollama.ollamaChatModel().chat(request);
         return chat.aiMessage().text();
+    }
+
+    /**
+     * 流式输出技术能够实现"边生成边展示"的效果，大幅降低感知延迟
+     * 重点是webflux以及返回的形式设置text/event-stream很重要
+     * 流式输出可以看控制台
+     * @param prompt
+     * @return
+     */
+    @GetMapping(value = "/stream",produces = "text/event-stream")
+    public Flux<String> stream(@RequestParam String prompt) {
+        return Flux.create(sink -> {
+            ollama.ollamaStreamingChatModel().chat(prompt, new StreamingChatResponseHandler() {
+                @Override
+                public void onPartialResponse(String partialResponse) {
+                    // 按照token输出
+                    sink.next(partialResponse);
+                }
+
+                @Override
+                public void onCompleteResponse(ChatResponse completeResponse) {
+                    sink.complete();
+                }
+
+                @Override
+                public void onError(Throwable error) {
+                    sink.error(error);
+                }
+            });
+        });
     }
 }
